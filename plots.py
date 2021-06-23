7e676c0b96d8f492de66cec5374a4670e12ba47b
JCatesPH <jmcates@email.arizona.edu>
2021-06-23 13:03:17 -0500

    Adding headers to cond and switching to intel

--- plots.py
+++ plots.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+itnum = str(sys.argv[1])
 
 #%% Read in input spectrum
 df = pd.read_table('DATA/InputSpectrum_1D.dat', header=0)
@@ -27,8 +28,9 @@ plt.savefig('figs/inSpectrum.png')
 
 #%% Read in plasma density
 df = pd.read_table('DATA/Ne_iter_5_Zpos_10630nm.dat', header=0)
-nedat = df.values
-nedat[:,1] = np.where(nedat[:,1] < 1e-8, 1e-8, nedat[:,1])
+nedat = df.values.astype(np.float)
+if np.isnan(nedat).any() != True:
+    nedat[:,1] = np.where(nedat[:,1] < 1e-8, 1e-8, nedat[:,1])
 
 
 #%%
@@ -40,6 +42,7 @@ plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
 plt.xlabel(r'$t$ [s]')
 plt.grid(which='major')
 #plt.show()
+plt.margins(x=0)
 plt.tight_layout()
 plt.savefig('figs/Ne.png')
 
@@ -54,13 +57,14 @@ plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
 plt.xlabel(r'$t$ [s]')
 plt.grid(which='major')
 #plt.show()
+plt.margins(x=0)
 plt.tight_layout()
 plt.savefig('figs/Ne_zoomed.png')
 ######################################
 
 
 #%% Read in forward-propagating pulse
-df = pd.read_table('DATA/Efield_iteration_5_Transmitted.dat', header=0)
+df = pd.read_table('DATA/Efield_iteration_' + itnum + '_Transmitted.dat', header=0)
 eFieldT = df.values
 
 
@@ -93,7 +97,7 @@ plt.savefig('figs/Et_transmitted_zoomed.png')
 
 
 #%% Read in backward-propagating pulse
-df = pd.read_table('DATA/Efield_iteration_5_Reflected.dat', header=0)
+df = pd.read_table('DATA/Efield_iteration_' + itnum + '_Reflected.dat', header=0)
 eFieldR = df.values
 
 
@@ -126,7 +130,7 @@ plt.savefig('figs/Et_reflected_zoomed.png')
 
 
 #%% Read in spectrum of transmitted pulse
-df = pd.read_table('DATA/Spectrum_iteration_5_Transmitted.dat', header=0)
+df = pd.read_table('DATA/Spectrum_iteration_' + itnum + '_Transmitted.dat', header=0)
 eSpectrumT = df.values
 
 
@@ -145,7 +149,7 @@ plt.savefig('figs/Ew_transmitted.png')
 
 
 #%% Read in spectrum of reflected pulse
-df = pd.read_table('DATA/Spectrum_iteration_5_Reflected.dat', header=0)
+df = pd.read_table('DATA/Spectrum_iteration_' + itnum + '_Reflected.dat', header=0)
 eSpectrumR = df.values
 
 
@@ -167,15 +171,21 @@ plt.savefig('figs/Ew_reflected.png')
 #%% Overlay transmitted and reflected spectra
 halfR = int(eSpectrumR.shape[0]/2)
 halfT = int(eSpectrumT.shape[0]/2)
+
 plt.clf()
 plt.figure(figsize=(8, 6))
-plt.semilogy(eSpectrumR[:halfR,0], eSpectrumR[:halfR,3], '-r', eSpectrumT[:halfT,0], eSpectrumT[:halfT,3], '-b')
-plt.title(r'Reflected pulse spectrum')
+plt.semilogy(eSpectrumT[:halfT,0], eSpectrumT[:halfT,3], '-b', 
+    eSpectrumR[:halfR,0], eSpectrumR[:halfR,3], '-r',
+    #inSpectrum[:halfR,0], inSpectrum[:halfR,3], '--k',
+    )
+plt.title(r'Transmitted and reflected pulse spectra')
 plt.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
+plt.legend(['Transmitted', 'Reflected'])
 plt.xlabel(r'$\omega$')
 plt.ylabel(r'$|E(\omega)|$')
 plt.grid(which='major')
 #plt.show()
+plt.margins(x=0)
 plt.tight_layout()
 plt.savefig('figs/Ew_both.png')
 
