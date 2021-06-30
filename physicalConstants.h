f95972a092f42b66f69bd76bdd665a40bb0465df
JCatesPH <jmcates@email.arizona.edu>
2021-06-30 16:44:29 -0500

    Adding units

--- physicalConstants.h
+++ physicalConstants.h
@@ -1,7 +1,7 @@
 #pragma once
 // constants in SI units
-const double cLight = 299792458; //speed of light
-const double epsilon_0 = 8.85418782e-12; //permittivity of free space
+const double cLight = 299792458; //speed of light [m / s]
+const double epsilon_0 = 8.85418782e-12; //permittivity of free space [W * s / V^2]
 const double charge_e = 1.602176634e-19; //electron charge
 const double mass_e = 9.10938356e-31; //electron mass
 const double u_Argon = 15.76; //argon potential
