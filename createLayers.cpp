de2adbbad25deb573af9e10873fd813e68d60260
JCatesPH <jmcates@email.arizona.edu>
2022-03-13 15:05:05 -0700

    Add function to build silica sample

--- createLayers.cpp
+++ createLayers.cpp
@@ -6,12 +6,18 @@
 void generateLayers(MaterialDB& theMaterialDB, Structure& theStructure)
 {
 	generatePlasmaTestMaterialsAndStructure(theMaterialDB, theStructure);
+	//generateSilicaMaterialsAndStructure(theMaterialDB, theStructure);
 	//generateDefectMaterialsAndStructure(theMaterialDB, theStructure);
 	//generateLayerTestMaterialsAndStructure(theMaterialDB,  theStructure);
 }
 
 void generateLayerTestMaterialsAndStructure(MaterialDB &theMaterialDB,  Structure &theStructure)
 {
+	const double n0_Silica = 1.4395;
+	const double n2_Silica = 2.3e-20; // [m^2 / W]
+	const double chi3_Silica = (4 / 3) * epsilon_0 * cLight * pow(n0_Silica, 2) * n2_Silica;
+	const double chi2_Silica = 0.0;
+
 	Material vacuum("Vacuum", n0_Vacuum, 0.0, 0.0, 0.0);
 	theMaterialDB.addMaterial(vacuum);
 	Material argon("Argon", n0_Argon, n2_Argon, chi2_Argon, chi3_Argon);
@@ -20,9 +26,39 @@ void generateLayerTestMaterialsAndStructure(MaterialDB &theMaterialDB,  Structur
 	theMaterialDB.addMaterial(mat1);
 	Material mat2("dieMat2", n0_Material2, n2_Material2, chi2_Material2, chi3_Material2);
 	theMaterialDB.addMaterial(mat2);
+	
 
 	theStructure.addLayer(theMaterialDB.getMaterialByName("Vacuum"), LHSsourceLayerThickness, zStepMaterial1);
     theStructure.addLayer(theMaterialDB.getMaterialByName("dieMat2"), sampleLayerThickness, zStepMaterial1);
+	theStructure.addLayer(theMaterialDB.getMaterialByName("Vacuum"), RHSbufferLayerThickness, zStepMaterial1);
+}
+
+void generateSilicaMaterialsAndStructure(MaterialDB &theMaterialDB,  Structure &theStructure)
+{
+	// Indices of refraction (quoted in 10.1103/PhysRevLett.110.115003)
+	const double n0_Silica = 1.4395;
+	const double n2_Silica = 2.3e-20; // [m^2 / W]
+	const double chi3_Silica = (4 / 3) * epsilon_0 * cLight * pow(n0_Silica, 2) * n2_Silica;
+	const double chi2_Silica = 0.0;
+
+	// Parameters for ionization in Durand paper
+	const double mpi_sigmaK = 1.0e-224; // [m^(28) W^(-14) s^(-1)] MPI cross section
+	const double mpi_k = 14.0; // Photons for MPI
+	const double sigmaBremsstrahlung = 4.7e-25; // [m^2] Bremsstrahlung cross section
+	const double silicaUi = 1.44e-18; // [J] = 9 [eV] Ionization potential of silica
+	const double recombTime = 150e-15; // [s] Recombination time
+
+	Material vacuum("Vacuum", n0_Vacuum, 0.0, 0.0, 0.0);
+	theMaterialDB.addMaterial(vacuum);
+
+	Material silica("Silica", n0_Silica, n2_Silica, chi2_Silica, chi3_Silica);
+	silica.setAsPlasmaMaterial(3, mpi_sigmaK, mpi_k, sigmaBremsstrahlung, silicaUi, recombTime);
+	theMaterialDB.addMaterial(silica);
+	
+
+	theStructure.addLayer(theMaterialDB.getMaterialByName("Vacuum"), LHSsourceLayerThickness, zStepMaterial1);
+    //theStructure.addLayer(theMaterialDB.getMaterialByName("dieMat2"), sampleLayerThickness, zStepMaterial1);
+	theStructure.addLayer(theMaterialDB.getMaterialByName("Silica"), sampleLayerThickness, zStepMaterial1);
 	theStructure.addLayer(theMaterialDB.getMaterialByName("Vacuum"), RHSbufferLayerThickness, zStepMaterial1);
 }
 
@@ -88,6 +124,9 @@ void generateDefectMaterialsAndStructure(MaterialDB& theMaterialDB, Structure& t
 
 void generatePlasmaTestMaterialsAndStructure(MaterialDB& theMaterialDB, Structure& theStructure)
 {
+	const double mpi_sigmaK = 3.4e-128;
+	const double mpi_k = 8.0;
+
 	double U_Ar = 15.759; // Ionization potential of Argon [eV]
 	//cout << sampleLayerThickness << endl;
 	Material vacuum("Vacuum", 1.0, 0.0, 0.0, 0.0);
--- createLayers.h
+++ createLayers.h
@@ -6,3 +6,4 @@ void generateLayerTestMaterialsAndStructure(MaterialDB &theMaterialDB,  Structur
 void generateApp1MaterialsAndStructure(MaterialDB &theMaterialDB,  Structure &theStructure);
 void generateDefectMaterialsAndStructure(MaterialDB& theMaterialDB, Structure& theStructure);
 void generatePlasmaTestMaterialsAndStructure(MaterialDB& theMaterialDB, Structure& theStructure);
+void generateSilicaMaterialsAndStructure(MaterialDB &theMaterialDB,  Structure &theStructure);
